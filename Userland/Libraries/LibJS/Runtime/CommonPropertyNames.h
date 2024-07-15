83f5a817ae6e6682f98b5fd295f791656818a583
Timothy Flynn <trflynn89@pm.me>
2024-07-15 12:30:26 -0400

    LibJS: Implement Uint8Array.prototype.toHex

--- Userland/Libraries/LibJS/Runtime/CommonPropertyNames.h
+++ Userland/Libraries/LibJS/Runtime/CommonPropertyNames.h
@@ -532,6 +532,7 @@ namespace JS {
     P(toExponential)                         \
     P(toFixed)                               \
     P(toGMTString)                           \
+    P(toHex)                                 \
     P(toInstant)                             \
     P(toISOString)                           \
     P(toJSON)                                \
