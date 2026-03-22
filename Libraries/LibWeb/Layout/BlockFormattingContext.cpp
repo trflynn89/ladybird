98e1774eff01c447ddca921cbfcd66845f1fa961
Psychpsyo <psychpsyo@gmail.com>
2026-03-22 21:20:56 +0100

    LibWeb: Avoid division by zero in multicol calculations the right way
    This got changed a while back to avoid a divide-by-0. That change was
    not following the spec at all and would've resulted in incorrect values
    being used, so this is the spec-compliant way of resolving that.
    
    I originally didn't add spec comments to this algorithm since it felt
    weird, but I hope these will demonstrate that this should not be changed
    on a whim until we can import the relevant WPT tests for this behavior.

--- Libraries/LibWeb/Layout/BlockFormattingContext.cpp
+++ Libraries/LibWeb/Layout/BlockFormattingContext.cpp
@@ -1604,24 +1604,45 @@ CSSPixels BlockFormattingContext::greatest_child_width(Box const& box) const
 Optional<int> BlockFormattingContext::determine_used_value_for_column_count(CSSPixels const& U) const
 {
     auto const& computed_values = root().computed_values();
+    // (01)  if ((column-width = auto) and (column-count = auto)) then
     if (computed_values.column_width().is_auto() && computed_values.column_count().is_auto()) {
+        // (02)      exit; /* not a multicol container */
         return {};
     }
+
+    // (03)  if column-width = auto then
     if (computed_values.column_width().is_auto()) {
+        // (04)      N := column-count
         return computed_values.column_count().value();
     }
+
     auto column_gap = get_column_gap_used_value_for_multicol(U);
-    auto column_width = computed_values.column_width().to_px(root(), U);
-    auto divisor = column_width + column_gap;
-    auto column_count = divisor == 0 ? 1 : max(1, ((U + column_gap) / divisor).to_int());
-    if (computed_values.column_count().is_auto())
-        return column_count;
-    return min(computed_values.column_count().value(), column_count);
+    auto column_width = get_column_width_used_value_for_multicol(U);
+
+    // (05)  else if column-count = auto then
+    if (computed_values.column_count().is_auto()) {
+        // (06)      N := max(1,
+        // (07)        floor((U + column-gap)/(column-width + column-gap)))
+        return max(1, ((U + column_gap) / (column_width + column_gap)).to_int());
+    }
+
+    // (08)  else
+    // (09)      N := min(column-count, max(1,
+    // (10)        floor((U + column-gap)/(column-width + column-gap))))
+    return min(computed_values.column_count().value(), max(1, ((U + column_gap) / (column_width + column_gap)).to_int()));
 }
 CSSPixels BlockFormattingContext::determine_used_value_for_column_width(CSSPixels const& U, int N) const
 {
     auto column_gap = get_column_gap_used_value_for_multicol(U);
+    // (11)  W := max(0, (U + column-gap)/N - column-gap)
     return max(CSSPixels(0), (U + column_gap) / N - column_gap);
+}
+
+// https://drafts.csswg.org/css-multicol-2/#cw
+CSSPixels BlockFormattingContext::get_column_width_used_value_for_multicol(CSSPixels const& U) const
+{
+    // Used values will be clamped to a minimum of '1px'.
+    return max(root().computed_values().column_width().to_px(root(), U), 1);
 }
 
 // https://www.w3.org/TR/css-align-3/#column-row-gap
