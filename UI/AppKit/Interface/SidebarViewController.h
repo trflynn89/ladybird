da4394531910fc34153de28790a9679c650440d3
Timothy Flynn <trflynn89@pm.me>
2026-07-11 10:01:52 -0400

    UI/AppKit: Add a sidebar view that lists vertical tabs
    Provide a native source-list table with favicon and title rows, hover
    close controls, middle-click handling, and a trailing New Tab row. The
    view reports user intent through blocks and remains inert until vertical
    presentation is installed.

---
+++ UI/AppKit/Interface/SidebarViewController.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2026, Timothy Flynn <trflynn89@pm.me>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#import <Cocoa/Cocoa.h>
+
+@class Tab;
+
+@interface SidebarViewController : NSViewController
+
+- (instancetype)initWithTabs:(NSArray<Tab*>*)tabs;
+- (void)reloadTabs;
+- (void)reloadTab:(Tab*)tab;
+- (void)selectTab:(Tab*)tab;
+
+@property (nonatomic, copy) NSArray<Tab*>* tabs;
+@property (nonatomic, assign, getter=isExpanded) BOOL expanded;
+@property (nonatomic, copy) void (^on_select_tab)(Tab*);
+@property (nonatomic, copy) void (^on_close_tab)(Tab*);
+@property (nonatomic, copy) void (^on_close_other_tabs)(Tab*);
+@property (nonatomic, copy) void (^on_move_tab_to_new_window)(Tab*);
+@property (nonatomic, copy) void (^on_new_tab)(void);
+@property (nonatomic, copy) void (^on_toggle_tab_audio_state)(Tab*);
+
+@end
